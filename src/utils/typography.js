bbd8d1c55b24910d24ee7540cccfde0ed61439d6
Ronald Libago <code@ronaldlibago.com>
2019-02-13 12:51:34 -0800

    DEPENDENCY: Add and configure Typography

---
+++ src/utils/typography.js
@@ -0,0 +1,17 @@
+import Typography from "typography";
+
+export default new Typography({
+  googleFonts: [
+    { name: "Muli", styles: ["700"] },
+    { name: "Spectral", styles: ["400", "400i", "700"] }
+  ],
+  headerFontFamily: [
+    "Muli",
+    "Century Gothic",
+    "CenturyGothic",
+    "URW Gothic L",
+    "Avant Garde",
+    "sans-serif"
+  ],
+  bodyFontFamily: ["Spectral", "Georgia", "serif"]
+});
