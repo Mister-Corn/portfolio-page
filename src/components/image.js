e3a2c275da9dccf18bd00cabbc98ff0c361db589
Ronald Libago <code@ronaldlibago.com>
2019-02-18 17:21:21 -0800

    Clean up console logs, Add comment regarding issue
        Issue: Initial loads of the page display normal behavior of images.
        However, subsequent refreshes would lead to intermittent loading/not
        loading of images.
    
        Apparent cause: If an image is already loaded, onLoad will not fire.
        Therefore, if the image is already loaded upon refresh, the image will
        remain invisible.
    
        Resolution: Institute a setTimeout that will delay adding the src to the
        img tag. Thus, when the component mounts, src will be , then shortly
        after, it will be the actual image. This will force a reload.

--- src/components/image.js
+++ src/components/image.js
@@ -7,19 +7,26 @@ const Image = props => {
   const { className, src, alt } = props;
   const [visible, setVisible] = useState(false);
   const [imgSrc, setImgSrc] = useState(null);
-  const handleImageLoaded = () => {
-    console.log("handleImageLoaded fired");
-    setVisible(true);
-  };
-  console.log("img visible state:", visible);
-
+  const handleImageLoaded = () => setVisible(true);
   useEffect(() => {
+    /* Reference: https://bugs.chromium.org/p/chromium/issues/detail?id=7731
+
+    Issue: Initial loads of the page display normal behavior of images. 
+    However, subsequent refreshes would lead to intermittent loading/not
+    loading of images.
+
+    Apparent cause: If an image is already loaded, onLoad will not fire.
+    Therefore, if the image is already loaded upon refresh, the image will 
+    remain invisible.
+
+    Resolution: Institute a setTimeout that will delay adding the src to the 
+    img tag. Thus, when the component mounts, src will be `null`, then shortly
+    after, it will be the actual image. This will force a reload.
+    */
     setTimeout(() => setImgSrc(src), 0);
   }, [src]);
 
-  console.log("imgSrc:", imgSrc);
   return (
-    // TODO: Fix state not being updated
     <img
       className={`${imageStyles.image} ${
         visible ? imageStyles.visible : imageStyles.invisible
