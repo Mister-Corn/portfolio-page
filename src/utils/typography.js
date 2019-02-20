c22e4e30bda479e2c8f5e17c1ec0d73e62b9ab32
mister-corn <code@ronaldlibago.com>
2019-02-20 15:39:30 -0800

    Minor change to font (Muli Black/Light)

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,18 +2,19 @@ import Typography from "typography";
 
 export default new Typography({
   googleFonts: [
-    { name: "Questrial", styles: ["700", "800"] },
+    { name: "Muli", styles: ["300", "900"] },
     { name: "Roboto", styles: ["400", "400i"] }
   ],
   headerFontFamily: [
     "Century Gothic",
     "CenturyGothic",
-    "Questrial",
+    "Muli",
     "URW Gothic L",
     "Avant Garde",
     "Arial",
     "sans-serif"
   ],
+  headerWeight: 900,
   bodyFontFamily: ["Roboto", "Arial", "sans-serif"],
   scaleRatio: 2.25,
   baseLineHeight: 1.1
